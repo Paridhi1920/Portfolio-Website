920b684492950682647af2462439007d630295ec
Paridhi Jain <paridhij30@gmail.com>
2025-08-02 22:20:11 +0530

    Modifying some files

--- backend/app.py
+++ backend/app.py
@@ -40,4 +40,7 @@ def download_resume():
     return send_from_directory(directory='static', path='ParidhiJain_Resume.pdf', as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port)
+
